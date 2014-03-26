827c49a2be502f9e591d80e0763815ebc9ef8926
Jazz-Aka-Amazing <Jazsingh12825@gmail.com>
2014-03-26 18:33:37 -0400

    Day 1

---
+++ GetVowel.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GetVowel.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                String input;
+                Console.WriteLine("Enter a Vowel")
+                input = Console.ReadLine().ToLower();
+
+        }
+    }
+}
---
+++ Sum.cs/Sum.cs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sum.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int n;
+            String input;
+
+            while (true)
+            {
+                Console.WriteLine("Enter a Number:");
+                input = Console.ReadLine();
+                n = Convert.ToInt32(input);
+
+                if (input == "999")
+                {
+                    break;
+                }
+
+                sum = n + sum;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
