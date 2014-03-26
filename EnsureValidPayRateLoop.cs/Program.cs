faa96ddebc97687f0e90d206647b27384245e2a7
Jazz-Aka-Amazing <Jazsingh12825@gmail.com>
2014-03-26 18:51:47 -0400

    Day 1-3

---
+++ EnsureValidPayRateLoop.cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnsureValidPayRateLoop.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double n;
+            String input;
+            while (true)
+            {
+                Console.WriteLine(" Enter a pay rate");
+                input = Console.ReadLine();
+                n = Convert.ToDouble(input);
+
+                sum = n + sum;
+                if (n < 5.65 || n > 49.99)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
+
